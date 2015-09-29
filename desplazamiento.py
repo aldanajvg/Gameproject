dde3ae9482dc9186342388c4129003f4b3717c5a
aldanajvg <aldanajvg@gmail.com>
2015-09-28 23:33:03 -0300

    Update desplazamiento.py

--- desplazamiento.py
+++ desplazamiento.py
@@ -9,7 +9,7 @@ fondo.agregar("SwampTree.png", y=300, velocidad=0.5)
 fondo.agregar("tallgrass.png", y=100, velocidad=0.9)
 fondo.agregar("grass.png", y=200, velocidad=2)
 
-p = pilas.actores.Shaolin(y=-180)
+p = pilas.actores.Shaolin(y=-210, x= -10000)
 
 def cambiar_posicion_camara():
     pilas.camara.x += (p.x - pilas.camara.x) /5
